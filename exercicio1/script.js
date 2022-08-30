8157a10d2321dcf33021e1a8956e9f8dc3c7115b
glauciasonda <glauciasonda@gmail.com>
2022-08-30 13:46:07 -0300

    Exercicios da aula 008

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+// a)
+let numPar = Number(prompt("Insira um número par"))
+
+// b)
+console.log("resto: ", numPar % 2)
+
+// c)
+/* O resto de qualquer divisão de um número par por  2 sempre será 0*/
+
+// d)
+/** O resto de qualquer divisão de um número impar por 2 sempre será 1 */
